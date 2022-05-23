5d3c835a7c42b9b5a946f1cf343986dfaaa60782
constanzaps <constanzapena@ug.uchile.cl>
2022-05-23 00:26:54 -0400

    Update f0_transacciones.py

--- Modelos/preprocessing/f0_transacciones.py
+++ Modelos/preprocessing/f0_transacciones.py
@@ -10,9 +10,10 @@ periodos=[201901, 201902, 201903, 201904, 201905, 201906, 201907,201908, 201909,
 
 #%% Creación de path ..
 print('Antes:',os.getcwd())
-# p = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-# os.chdir(path)
-# print('desp:',os.getcwd())#os.listdir()
+p = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+path='C:/Users/Asus/Documents/GitHub/ANN_Itau/'
+os.chdir(path)
+print('desp:',os.getcwd())#os.listdir()
 
 #%% OBTENER DATASET ORIGINAL, MEZLCAMOS TRAIN Y TEST
 #Para crear los .csv
@@ -48,7 +49,7 @@ del df_test
 #     data[f"P_{filename}"] = pd.read_csv('Datos/raw/transaction_subset/'+f"P_{filename}"+'.csv')
 #     size[f"P_{filename}"]=data[f"P_{filename}"].shape[0]
 
-#%%
+#%% Definir tipos
 def definir_tipos(dataset):    
     dataset.id = dataset.id.astype('object') #Lo pasamos a str
     dataset.Signo = dataset.Signo.astype('object') #tb
@@ -61,6 +62,11 @@ for i in list(data.keys()):
     definir_tipos(data[i])
 ncol=data['P_201906'].shape[1]
 print(f'El dataset tiene {sum(dim.values())} filas y {ncol} columnas')
+#%%
+datita=data['P_201906'].sample(5)
+del datita
+del i
+del ncol
 
 #%% Añadimos variables de transacciones por id y producto, de transacciones de id por periodo,
 # y 
@@ -69,12 +75,12 @@ new_data={}
 #Primero hacemos variables base
 for i in data_enumerate.keys(): 
     #Variable numerica de cuantas transacciones realizó un id por producto en periodo actual
-    data[f"P_{data_enumerate[i]}"].loc[:, ['id-producto']]=data[f"P_{data_enumerate[i]}"]['id'].astype(str)+"-"+data[f"P_{data_enumerate[i]}"]['Id_Producto']
-    cuenta=data[f"P_{data_enumerate[i]}"].groupby(['id-producto'])['Monto'].count().to_frame() #Contar las transacciones
-    cuenta['id-producto'] = cuenta.index #indice a columna para hacer merge
+    data[f"P_{data_enumerate[i]}"].loc[:, ['id-producto-tipo']]=data[f"P_{data_enumerate[i]}"]['id'].astype(str)+"-"+data[f"P_{data_enumerate[i]}"]['Producto-Tipo']
+    cuenta=data[f"P_{data_enumerate[i]}"].groupby(['id-producto-tipo'])['Monto'].count().to_frame() #Contar las transacciones
+    cuenta['id-producto-tipo'] = cuenta.index #indice a columna para hacer merge
     cuenta.index.names = ['index'] #le cambiamos el nombre
     cuenta=cuenta.rename(columns={'Monto': 't_i-0'})#transacciones periodo actual
-    new_data[f"P_{data_enumerate[i]}"]=pd.merge(data[f"P_{data_enumerate[i]}"],cuenta,on='id-producto',how='left')#N° transacciones mes actual
+    new_data[f"P_{data_enumerate[i]}"]=pd.merge(data[f"P_{data_enumerate[i]}"],cuenta,on='id-producto-tipo',how='left')#N° transacciones mes actual
     
     #Agregamos el total de transacciones en el mes en curso
     cuenta2=new_data[f"P_{data_enumerate[i]}"].groupby(['id'])['Monto'].count().to_frame() #Total de transacciones en el mes en curso
@@ -85,13 +91,13 @@ for i in data_enumerate.keys():
     
     # Lo pasamos a probabilidades, es decir, dividimos t_0 (transacciones del mes actual) en las transacciones totales
     #Tenemos esta probabilidad en el periodo actual
-    new_data[f"P_{data_enumerate[i]}"]['P Producto']=new_data[f"P_{data_enumerate[i]}"]['t_i-0']/new_data[f"P_{data_enumerate[i]}"]['TotalTMes']
+    new_data[f"P_{data_enumerate[i]}"]['P PT']=new_data[f"P_{data_enumerate[i]}"]['t_i-0']/new_data[f"P_{data_enumerate[i]}"]['TotalTMes']
     #Y ponemos la probabilidad que tenía el periodo anterior
     if i>=2:    
-        new_data[f"P_{data_enumerate[i]}"]['P Producto i-1']=new_data[f"P_{data_enumerate[i-1]}"]['t_i-0']/new_data[f"P_{data_enumerate[i-1]}"]['TotalTMes']
+        new_data[f"P_{data_enumerate[i]}"]['P PT i-1']=new_data[f"P_{data_enumerate[i-1]}"]['t_i-0']/new_data[f"P_{data_enumerate[i-1]}"]['TotalTMes']
     else: #Igual creamos la columna pero vacía 
-        new_data[f"P_{data_enumerate[i]}"]['P Producto i-1']=np.nan
-        
+        new_data[f"P_{data_enumerate[i]}"]['P PT i-1']=np.nan
+
 #%%
 
 #Variable numerica de cuantas transacciones realizó en periodo anterior
