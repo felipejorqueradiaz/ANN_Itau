18405b4608a05a9949f64129907dddd864c424a1
Felipe Jorquera <felipejorqueradiaz@gmail.com>
2022-06-11 15:25:48 -0400

    Update nbayes.py

--- Modelos/models/nbayes.py
+++ Modelos/models/nbayes.py
@@ -12,7 +12,9 @@ path='C:/Users/Asus/Documents/GitHub/ANN_Itau'
 
 
 os.chdir(path)
-# target= pd.read_pickle('Datos/final/Target.pkl', compression= 'zip')
+
+#%%
+target= pd.read_pickle('Datos/final/Target.pkl', compression= 'zip')
 #%%
 
 product_list = ['A-A',
@@ -65,36 +67,35 @@ pred = pd.concat([pred, id_per.reset_index(drop = True)], axis = 1, ignore_index
 real.columns = product_list + ['id', 'Periodo']
 pred.columns = product_list + ['id', 'Periodo']
 #%%
+
+
 prod_vector = np.array(product_list)
 corte = 0.5
-for mes in real.Periodo.unique():
-    real_temp = real[real['Periodo'] == mes]
-    pred_temp = pred[pred['Periodo'] == mes]
-    
-    d_true = real_temp[product_list].to_numpy(copy = True)
-    true_sort_mask = d_true.argsort()
-    v_true = np.where(d_true, prod_vector, 'nulo')
-    v_true = np.take_along_axis(v_true,true_sort_mask,axis=1).tolist()
 
+
+for mes in real.Periodo.unique():
+    real_temp = target[target['Periodo'] == mes].reset_index(drop=True)
+    list_true = real_temp['productos'].to_numpy(copy = True).tolist()
+    
+ 
+    pred_temp = pred[pred['Periodo'] == mes]
     d_pred = pred_temp[product_list].to_numpy(copy = True)
     pred_sort_mask = d_pred.argsort()
     d2_pred = np.where(d_pred <= corte, 0, 1)
     v_pred = np.where(d2_pred, prod_vector, 'nulo')
-    v_pred = np.take_along_axis(v_pred,pred_sort_mask,axis=1).tolist()
-    
-    print('EL MAP5 para el mes {} es:'.format(mes), metrics.mapk(v_true, v_pred, 5), '\n\n')
+    v_pred = np.take_along_axis(v_pred,pred_sort_mask,axis=1)[:, [4, 3, 2, 1, 0]].tolist()
+    v_pred = [[valor for valor in lista if valor!='nulo'] for lista in v_pred]
+    print('EL MAP5 para el mes {} es:'.format(mes), metrics.mapk(list_true, v_pred, 5), '\n\n')
 
 
-d_true = real[product_list].to_numpy(copy = True)
-true_sort_mask = d_true.argsort()
-v_true = np.where(d_true, prod_vector, 'nulo')
-v_true = np.take_along_axis(v_true,true_sort_mask,axis=1)[:, [4, 3, 2, 1, 0]].tolist()
+list_true = target.reset_index(drop=True)['productos'].to_numpy(copy = True).tolist()
 
-d_pred = pred[product_list].to_numpy(copy = True)
+pred_temp = pred.copy()
+d_pred = pred_temp[product_list].to_numpy(copy = True)
 pred_sort_mask = d_pred.argsort()
 d2_pred = np.where(d_pred <= corte, 0, 1)
 v_pred = np.where(d2_pred, prod_vector, 'nulo')
 v_pred = np.take_along_axis(v_pred,pred_sort_mask,axis=1)[:, [4, 3, 2, 1, 0]].tolist()
-
-np.where(XXXXXX <= corte, 0, 1)
+v_pred = [[valor for valor in lista if valor!='nulo'] for lista in v_pred]
+#np.where(XXXXXX <= corte, 0, 1)
 print('EL MAP5 en general es:'.format(mes), metrics.mapk(v_true, v_pred, 5), '\n\n')
